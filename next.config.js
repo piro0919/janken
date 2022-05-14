ba0d99f02133050fca0529958231a0fa8e05f692
piro <piro.haniwa@gmail.com>
2022-05-15 05:27:28 +0900

    fix: enable pwa

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
+const withPWA = require("next-pwa");
 const { i18n } = require("./next-i18next.config");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   i18n,
   pwa: {
     dest: "public",
-    disable: process.env.NODE_ENV === "development",
   },
   reactStrictMode: true,
-};
+});
 
 module.exports = nextConfig;
